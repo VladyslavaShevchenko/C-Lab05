862e608cb6fa05b6c4c73f6e6d71d40000705806
VladyslavaShevchenko <37338393+VladyslavaShevchenko@users.noreply.github.com>
2019-04-06 11:09:41 +0300

    Add files via upload

---
+++ Lab04/InfoView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace Lab04
+{
+    /// <summary>
+    /// Логика взаимодействия для InfoView.xaml
+    /// </summary>
+    internal partial class InfoView : UserControl
+    {
+        internal InfoView(System.Diagnostics.Process process)
+        {
+            InitializeComponent();
+            DataContext = new InfoViewModel(process.Modules, process.Threads);
+        }
+    }
+}
---
+++ Lab04/InfoWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace Lab04
+{
+    /// <summary>
+    /// Interaction logic for InfoWindow.xaml
+    /// </summary>
+    internal partial class InfoWindow : Window
+    {
+        private InfoView _infoView;
+
+        internal InfoWindow(System.Diagnostics.Process process)
+        {
+            InitializeComponent();
+            Title = $"{process.ProcessName} Info";
+            ShowInfoView(process);
+        }
+
+        private void ShowInfoView(System.Diagnostics.Process process)
+        {
+            MainGrid.Children.Clear();
+            if (_infoView == null)
+                _infoView = new InfoView(process);
+            MainGrid.Children.Add(_infoView);
+        }
+    }
+}
